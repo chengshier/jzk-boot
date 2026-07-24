ca518a9d404db6f5443ce43276cb1ce84acad04f
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-24 16:07:57 +0800

    feat: add receive exception dao

---
+++ crmeb-service/src/main/java/com/zbkj/service/dao/jiuzhoukang/JkTradeReceiveExceptionDao.java
@@ -0,0 +1,6 @@
+package com.zbkj.service.dao.jiuzhoukang;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zbkj.common.model.jiuzhoukang.JkTradeReceiveException;
+
+public interface JkTradeReceiveExceptionDao extends BaseMapper<JkTradeReceiveException> { }
