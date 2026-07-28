22db8a817a309e8d289f4324fbf656849288a087
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-28 23:11:42 +0800

    feat: 增加库存盘点明细 DAO

---
+++ crmeb-service/src/main/java/com/zbkj/service/dao/jiuzhoukang/JkStockCheckItemDao.java
@@ -0,0 +1,6 @@
+package com.zbkj.service.dao.jiuzhoukang;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zbkj.common.model.jiuzhoukang.JkStockCheckItem;
+
+public interface JkStockCheckItemDao extends BaseMapper<JkStockCheckItem> { }
