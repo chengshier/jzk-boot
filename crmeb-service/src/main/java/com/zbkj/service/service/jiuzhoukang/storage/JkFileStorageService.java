0fc16d203e625a671991563f5256bd2fe22b9b2e
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-29 00:08:40 +0800

    feat: 增加统一文件存储服务接口

---
+++ crmeb-service/src/main/java/com/zbkj/service/service/jiuzhoukang/storage/JkFileStorageService.java
@@ -0,0 +1,14 @@
+package com.zbkj.service.service.jiuzhoukang.storage;
+
+import com.zbkj.common.model.jiuzhoukang.JkFileObject;
+import com.zbkj.common.response.jiuzhoukang.JkFileObjectResponse;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+public interface JkFileStorageService {
+    JkFileObjectResponse store(MultipartFile file, String businessType, Long businessId, Long ownerUserId, String accessLevel);
+    byte[] read(Long fileId, Long viewerUserId, boolean admin);
+    JkFileObject require(Long fileId);
+    Map<String, Object> status();
+}
