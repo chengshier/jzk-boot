5d7a1a0b01464c47cc8ce1e699744a946796044e
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-28 22:58:24 +0800

    fix: 支持佣金试算请求链式构建

--- crmeb-common/src/main/java/com/zbkj/common/request/jiuzhoukang/JkCommissionRuleTrialRequest.java
+++ crmeb-common/src/main/java/com/zbkj/common/request/jiuzhoukang/JkCommissionRuleTrialRequest.java
@@ -1,12 +1,14 @@
 package com.zbkj.common.request.jiuzhoukang;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
+@Accessors(chain = true)
 public class JkCommissionRuleTrialRequest {
     private Long ruleId;
     @NotNull private String sourceType;
