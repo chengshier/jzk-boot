ada3bb36405c8412a5664ca76217eee55edde4fb
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-29 00:11:41 +0800

    feat: 增加订阅消息任务模型

---
+++ crmeb-common/src/main/java/com/zbkj/common/model/jiuzhoukang/JkSubscriptionTask.java
@@ -0,0 +1,34 @@
+package com.zbkj.common.model.jiuzhoukang;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Accessors(chain = true)
+@TableName("jk_subscription_task")
+public class JkSubscriptionTask implements Serializable {
+    @TableId(value="id",type=IdType.AUTO) private Long id;
+    private String taskNo;
+    private String templateCode;
+    private String businessType;
+    private Long businessId;
+    private Long receiverUserId;
+    private String pagePath;
+    private String payloadJson;
+    private String status;
+    private Integer retryCount;
+    private Integer maxRetryCount;
+    private Date nextRetryTime;
+    private Date sentAt;
+    private String errorMessage;
+    private String requestNo;
+    private Boolean isDeleted;
+    private Date createTime;
+    private Date updateTime;
+}
