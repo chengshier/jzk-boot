ee4da3a5ed534a323ca8aab59f6905f17432a89c
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-28 23:37:06 +0800

    feat: 增加推广统计 DAO

---
+++ crmeb-service/src/main/java/com/zbkj/service/dao/jiuzhoukang/JkPromotionStatDao.java
@@ -0,0 +1,4 @@
+package com.zbkj.service.dao.jiuzhoukang;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zbkj.common.model.jiuzhoukang.JkPromotionStat;
+public interface JkPromotionStatDao extends BaseMapper<JkPromotionStat> { }
