ffb1fddfbd55bfa92d3e1a940ca24ea65b3d2501
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-28 21:45:29 +0800

    feat: 新增关系人数规则 DAO

---
+++ crmeb-service/src/main/java/com/zbkj/service/dao/jiuzhoukang/JkRelationLimitRuleDao.java
@@ -0,0 +1,7 @@
+package com.zbkj.service.dao.jiuzhoukang;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zbkj.common.model.jiuzhoukang.JkRelationLimitRule;
+
+public interface JkRelationLimitRuleDao extends BaseMapper<JkRelationLimitRule> {
+}
