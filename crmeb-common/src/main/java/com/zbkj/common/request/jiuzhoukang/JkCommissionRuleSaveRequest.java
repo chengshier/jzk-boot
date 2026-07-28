ba76f0d6cdb607f0b5f4814d76dc05b2932216f4
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-28 23:01:38 +0800

    feat: 扩展 V3.1 佣金规则草稿参数

--- crmeb-common/src/main/java/com/zbkj/common/request/jiuzhoukang/JkCommissionRuleSaveRequest.java
+++ crmeb-common/src/main/java/com/zbkj/common/request/jiuzhoukang/JkCommissionRuleSaveRequest.java
@@ -1,16 +1,40 @@
 package com.zbkj.common.request.jiuzhoukang;
 
 import lombok.Data;
+
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
 public class JkCommissionRuleSaveRequest implements Serializable {
     private Long id;
+    private Long planId;
+    private String ruleCode;
     private String ruleName;
     private Integer ruleVersion;
+    private String rewardType;
     private String sourceType;
+    private String performanceType;
     private String receiverRoleCode;
+    private String beneficiaryType;
+    private String baseType;
+    private String calculationType;
+    private BigDecimal rate;
+    private BigDecimal fixedAmount;
+    private BigDecimal unitAmount;
+    private String triggerTiming;
+    private Integer settleDelayDays;
+    private String stackGroup;
+    private String stackPolicy;
+    private Integer priority;
+    private BigDecimal perOrderCap;
+    private BigDecimal perUserPeriodCap;
+    private BigDecimal totalBudget;
+    private Boolean requiresRegisteredCustomer;
+    private Boolean requiresVoucher;
+    private Boolean requiresAudit;
+    private String receiverRoleCodeLegacy;
     private String regionCode;
     private Date effectiveTime;
     private Date expireTime;
