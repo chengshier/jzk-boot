05e11a90a02e74e0831449070f3662c3f6487a44
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-29 18:51:48 +0800

    feat(jzk-v3.1-c): 定义真实业务单据佣金试算服务

---
+++ crmeb-service/src/main/java/com/zbkj/service/service/jiuzhoukang/commission/JkCommissionSourceTrialService.java
@@ -0,0 +1,9 @@
+package com.zbkj.service.service.jiuzhoukang.commission;
+
+import com.zbkj.common.request.jiuzhoukang.JkCommissionSourceTrialRequest;
+
+import java.util.Map;
+
+public interface JkCommissionSourceTrialService {
+    Map<String, Object> trial(JkCommissionSourceTrialRequest request);
+}
