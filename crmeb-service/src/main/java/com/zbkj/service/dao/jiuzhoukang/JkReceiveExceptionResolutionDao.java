c34c3a357c7b992e4ecbaf07e08fed20a2d93766
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-29 09:08:30 +0800

    feat: 增加异常收货V2方案DAO

---
+++ crmeb-service/src/main/java/com/zbkj/service/dao/jiuzhoukang/JkReceiveExceptionResolutionDao.java
@@ -0,0 +1,9 @@
+package com.zbkj.service.dao.jiuzhoukang;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zbkj.common.model.jiuzhoukang.JkReceiveExceptionResolution;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface JkReceiveExceptionResolutionDao extends BaseMapper<JkReceiveExceptionResolution> {
+}
