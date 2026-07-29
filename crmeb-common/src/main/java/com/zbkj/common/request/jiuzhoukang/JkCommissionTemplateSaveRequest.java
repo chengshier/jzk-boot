f7006e744fed5de9df06acd79814488c9ce9b38a
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-29 18:46:45 +0800

    feat(jzk-v3.1-c): 增加业务模板驱动的奖励规则请求

---
+++ crmeb-common/src/main/java/com/zbkj/common/request/jiuzhoukang/JkCommissionTemplateSaveRequest.java
@@ -0,0 +1,29 @@
+package com.zbkj.common.request.jiuzhoukang;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+/** 普通运营使用的业务模板表单；技术枚举由服务端模板映射。 */
+@Data
+public class JkCommissionTemplateSaveRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long ruleId;
+    @NotNull(message = "商业方案不能为空") private Long planId;
+    @NotBlank(message = "奖励模板不能为空") private String templateCode;
+    @NotBlank(message = "规则名称不能为空") private String ruleName;
+    @NotBlank(message = "适用受益身份不能为空") private String receiverRoleCode;
+    @NotBlank(message = "奖励方式不能为空") private String rewardMode;
+    @NotNull(message = "奖励数值不能为空") private BigDecimal rewardValue;
+    private List<Integer> productIds;
+    private List<String> regionCodes;
+    private BigDecimal perOrderCap;
+    private BigDecimal perUserPeriodCap;
+    private Integer settleDelayDays;
+    private String remark;
+}
