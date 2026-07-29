95fdbbd0b10b5fe5eaff4b713fb9cde206b912cf
chengshier <102338125+chengshier@users.noreply.github.com>
2026-07-29 19:18:45 +0800

    fix(jzk-v3.1-d): 使用0表示规则预算公共占用行确保唯一约束

--- crmeb-service/src/main/java/com/zbkj/service/dao/jiuzhoukang/JkCommissionLimitUsageDao.java
+++ crmeb-service/src/main/java/com/zbkj/service/dao/jiuzhoukang/JkCommissionLimitUsageDao.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Select;
 
 public interface JkCommissionLimitUsageDao extends BaseMapper<JkCommissionLimitUsage> {
     @Select("SELECT * FROM jk_commission_limit_usage WHERE usage_type=#{usageType} AND rule_id=#{ruleId} " +
-            "AND ((#{userId} IS NULL AND user_id IS NULL) OR user_id=#{userId}) AND period_key=#{periodKey} LIMIT 1 FOR UPDATE")
+            "AND user_id=#{userId} AND period_key=#{periodKey} LIMIT 1 FOR UPDATE")
     JkCommissionLimitUsage selectForUpdate(@Param("usageType") String usageType,
                                            @Param("ruleId") Long ruleId,
                                            @Param("userId") Long userId,
